b3b7e8da96c15c2b1f1757a77d7c3426002b7d53
RoshanCyriac <roshancyriac.k@gmail.com>
2025-08-12 02:40:33 +0530

    eror

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['www.fosscusat.org', 'fosscusat.org', 'localhost'],
+    host: '0.0.0.0',
+    port: 3000
+  },
+  preview: {
+    allowedHosts: ['www.fosscusat.org', 'fosscusat.org', 'localhost'],
+    host: '0.0.0.0',
+    port: 3000
+  }
 })
